cd8f03f0791b7e3ca217393506e5ffda7c698c76
github username <github email address>
2017-04-12 14:28:28 -0400

    completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,29 +1,49 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
     # create a new Student object given a row from the database
   end
 
   def self.all
+    sql = <<-SQL
+    SELECT * FROM students
+    SQL
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
   end
 
   def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE name = (?) LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).collect do |row|
+      self.new_from_db(row)
+    end[0]
     # find the student in the database given a name
     # return a new instance of the Student class
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
@@ -40,4 +60,55 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+    SELECT COUNT(grade) FROM students WHERE grade="9th"
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_x_students_in_grade_10(number)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = 10
+    ORDER BY id ASC
+    LIMIT (?)
+    SQL
+    DB[:conn].execute(sql, number).collect do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = 10
+    ORDER BY id ASC
+    SQL
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.all_students_in_grade_x(grade)
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = (?)
+    SQL
+    DB[:conn].execute(sql, grade).collect do |row|
+      self.new_from_db(row)
+    end
+  end
+
+
 end
